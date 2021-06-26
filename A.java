fef471b08511a6aa3187d422d2351c09d3be0689
KrishnaPrabha13 <86238794+KrishnaPrabha13@users.noreply.github.com>
2021-06-26 22:49:07 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+package com.p1;
+
+public class A 
+{
+	public void display()
+	{
+		System.out.println("This is Class A");
+	}
+
+}
---
+++ B.java
@@ -0,0 +1,11 @@
+package com.p2;
+import com.p1.A;
+
+public class B {
+	public static void main(String [] args)
+	{
+		A obj = new A();
+		obj.display();
+	}
+}
+
